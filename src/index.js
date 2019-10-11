96b15dd4668e14d3665beeb9a94af93129bcdb36
mkkim <mkkim@zensystems.co.kr>
2019-10-11 11:56:58 +0900

    Guides/Code Splitting/Dynamic Imports #1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import _ from 'lodash';
+function getComponent() {
+  /**
+   * 해당 js 파일을 HTML 에서 사용시
+   * 필요한 모듈을 담고 있는 chunk file 을 동적으로 HTML 에 선언한다.
+   */
+  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
+    const element = document.createElement('div');
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    return element;
 
-  btn.innerHTML = 'Click me and check the console!';
-
-  element.appendChild(btn);
-
-  return element;
+  }).catch(error => 'An error occurred while loading the component');
 }
 
-console.log(_.join(['index.js', 'module', 'loaded!', 'Removed plugins'], ' '));
-document.body.appendChild(component());
+getComponent().then(component => {
+  document.body.appendChild(component);
+});
