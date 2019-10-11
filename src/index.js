0c81f9ea1df9aa96aad06e23bd8eee537c44cb60
mkkim <mkkim@zensystems.co.kr>
2019-10-11 14:04:28 +0900

    Guides/Code Splitting/Dynamic Imports #2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-function getComponent() {
+async function getComponent() {
   /**
    * 해당 js 파일을 HTML 에서 사용시
    * 필요한 모듈을 담고 있는 chunk file 을 동적으로 HTML 에 선언한다.
    */
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
+  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
-
-  }).catch(error => 'An error occurred while loading the component');
+  return element;
 }
 
 getComponent().then(component => {
